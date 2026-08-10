79f88f4eeb491ab2c7332a3955649272262fb595
Nguyễn Tiến Đạt <tiendat2k3.dev@gmail.com>
2026-08-10 21:34:47 +0700

    Update not-found.tsx

--- src/not-found.tsx
+++ src/not-found.tsx
@@ -1,4 +1,12 @@
+import Link from "next/link";
+
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>404 - Khong tim thay trang</h1>
+      <Link href="/">Quay ve home</Link>
+    </div>
+  );
 };
+
 export default NotFound;
